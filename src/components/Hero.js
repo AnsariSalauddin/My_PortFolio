c73f92191ea2994961b21b7b162e53a1d68ead1c
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-10-12 08:26:11 +0530

    add image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -72,12 +72,9 @@ const Hero = () => {
 
           <div className="hero-image">
             <div className="image-container">
-              <div className="profile-image">
-                <div className="image-placeholder">
-                    <span>image</span>
-                  {/* <span className='image-placeholder-img'><img src={require('../assest/SalauddinImage.jpg')}  alt="Profile" /></span> */}
-                </div>
-              </div>
+               <div className="profile-image">
+                 <img src={require('../assets/SalauddinImage.jpg')} alt="Salauddin Ansari" className="profile-img" />
+               </div>
               <div className="image-decoration"></div>
             </div>
           </div>
